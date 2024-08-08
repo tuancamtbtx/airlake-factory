f5f28d9385ef954b97214a4b8a5a5d44ddb22cbc
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-08-08 21:19:25 +0700

    [add] config setup package

--- airfactory/__init__.py
+++ airfactory/__init__.py
@@ -5,3 +5,15 @@ _ROOT = os.path.dirname(os.path.dirname(__file__))
 
 def resolve_root(*arg: str):
   return os.path.join(_ROOT, *arg)
+
+## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
+
+__version__ = "v1.0.0"
+
+def get_provider_info():
+    return {
+        "package-name": "airfactory", # Required
+        "name": "airfactory", # Required
+        "description": "Dag Factory for Apache Airflow.", # Required
+        "versions": [__version__] # Required
+    }
